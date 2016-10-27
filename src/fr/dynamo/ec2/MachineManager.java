c2b6f47684389a589797cdd9271ae54aae32008e
Florian Roesler <florian.roesler@wooga.net>
2016-10-27 11:53:58 +0200

    rewrote MachineManager for simpler access including property file parsing

--- src/fr/dynamo/ec2/MachineManager.java
+++ src/fr/dynamo/ec2/MachineManager.java
@@ -1,5 +1,7 @@
 package fr.dynamo.ec2;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -9,8 +11,14 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.profile.ProfilesConfigFile;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
@@ -23,31 +31,35 @@ import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 
 public class MachineManager {
 
-  private static MachineManager instance;
+  private AmazonEC2 client;
+  private String imageId;
+  private String cpuInstanceType;
+  private String gpuInstanceType;
+  private String keyName;
+  private String securityGroup;
 
-  private static AmazonEC2 client;
+  public MachineManager(String propertiesPath){
+    AWSCredentialsProvider credentials = new ProfileCredentialsProvider(new ProfilesConfigFile(propertiesPath), "default");
+    this.client = AmazonEC2ClientBuilder.standard().withCredentials(credentials).withRegion(Regions.EU_CENTRAL_1).build();
 
-  private MachineManager(){
-  }
-
-  public static MachineManager instance(){
-    if(client == null){
-      throw new IllegalStateException("MachineManager requires to have a valid Amazon EC2 client set before initialization.");
+    Properties prop = new Properties();
+    try {
+      prop.load(new FileInputStream(new File(propertiesPath)));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    if(instance == null){
-      instance = new MachineManager();
-    }
+    imageId = prop.getProperty("image_id");
+    cpuInstanceType = prop.getProperty("cpu_instance_type");
+    gpuInstanceType = prop.getProperty("gpu_instance_type");
+    keyName = prop.getProperty("key_name");
+    securityGroup = prop.getProperty("security_group");
 
-    return instance;
   }
+
 
   public AmazonEC2 getClient() {
     return client;
-  }
-
-  public static void setClient(AmazonEC2 c) {
-    client = c;
   }
 
   public List<String> bookInstance(){
@@ -55,12 +67,12 @@ public class MachineManager {
 
     RunInstancesRequest runInstancesRequest = new RunInstancesRequest();
 
-    runInstancesRequest.withImageId("ami-26c43149")
-                       .withInstanceType("t2.micro")
-                       .withMinCount(1)
-                       .withMaxCount(1)
-                       .withKeyName("opencl-host")
-                       .withSecurityGroups("opencl-host-WebServerSecurityGroup-192ER9ITPN3RE");
+    runInstancesRequest.withImageId(imageId)
+    .withInstanceType(cpuInstanceType)
+    .withMinCount(1)
+    .withMaxCount(1)
+    .withKeyName(keyName)
+    .withSecurityGroups(securityGroup);
 
     Date before = new Date();
     RunInstancesResult runInstancesResult = client.runInstances(runInstancesRequest);
@@ -68,26 +80,10 @@ public class MachineManager {
     System.out.println(runInstancesResult.getReservation().getInstances().size() + " instances launched.");
     System.out.println("Waiting for instances to finish boot.");
 
-    InstanceState pendingState = new InstanceState().withName(InstanceStateName.Pending.name());
 
     List<Instance> bookedInstances = runInstancesResult.getReservation().getInstances();
 
-    for(Instance instance:bookedInstances){
-      boolean pending = true;
-      while(pending){
-        try {
-          Thread.sleep(500);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        DescribeInstancesRequest request = new DescribeInstancesRequest().withInstanceIds(instance.getInstanceId());
-        DescribeInstancesResult describeInstanceResult = client.describeInstances(request);
-
-        if(describeInstanceResult.getReservations().get(0).getInstances().get(0).getState().getCode() != pendingState.getCode()){
-          pending = false;
-        }
-      }
-    }
+    blockUntilRunning(bookedInstances, 60000);
 
     Date after = new Date();
     System.out.println(after.getTime() - before.getTime() + " ms launch time.");
@@ -110,10 +106,10 @@ public class MachineManager {
 
     List<Instance> instances;
     for(Reservation res : reservations){
-        instances = res.getInstances();
-        for(Instance ins : instances){
-          publicIps.put(ins.getInstanceId(),ins.getPublicIpAddress());
-        }
+      instances = res.getInstances();
+      for(Instance ins : instances){
+        publicIps.put(ins.getInstanceId(),ins.getPublicIpAddress());
+      }
     }
 
     return publicIps;
@@ -121,25 +117,54 @@ public class MachineManager {
 
 
   public void terminateInstances(List<String> instanceIds){
+    System.out.println("Terminating instances.");
     TerminateInstancesRequest request = new TerminateInstancesRequest(instanceIds);
     client.terminateInstances(request);
+    System.out.println("Instances terminated.");
   }
 
 
+  private boolean blockUntilRunning(List<Instance> bookedInstances, long timeout){
+    long start = System.currentTimeMillis();
+    final InstanceState pendingState = new InstanceState().withName(InstanceStateName.Pending.name());
+
+    for(Instance instance:bookedInstances){
+      boolean pending = true;
+      while(pending){
+        if(System.currentTimeMillis() - start > timeout) return false;
+
+        try {
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        DescribeInstancesRequest request = new DescribeInstancesRequest().withInstanceIds(instance.getInstanceId());
+        DescribeInstancesResult describeInstanceResult = client.describeInstances(request);
+
+        if(describeInstanceResult.getReservations().get(0).getInstances().get(0).getState().getCode() != pendingState.getCode()){
+          pending = false;
+        }
+      }
+    }
+    return true;
+  }
+
   public boolean blockUntilReachable(Collection<String> instanceIps, long timeout) {
+    System.out.println("Waiting for Instances to be reachable via SSH.");
     long start = System.currentTimeMillis();
     outer:for(String ip:instanceIps){
       while(true){
         if(System.currentTimeMillis() - start > timeout) return false;
         try {
           try (Socket soc = new Socket()) {
-              soc.connect(new InetSocketAddress(ip, 22), 500);
+            soc.connect(new InetSocketAddress(ip, 22), 500);
           }
           continue outer;
         } catch (IOException ex) {
         }
       }
     }
+    System.out.println("Instances are reachable via SSH.");
     return true;
   }
 
