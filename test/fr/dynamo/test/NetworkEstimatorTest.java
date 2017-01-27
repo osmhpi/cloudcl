75bc28e9c5aff3c8ac6a9ce5fa335702a1b212ec
Florian Roesler <florian.roesler@wooga.net>
2017-01-27 12:27:47 +0100

    added test for data transfer tracking

--- test/fr/dynamo/test/NetworkEstimatorTest.java
+++ test/fr/dynamo/test/NetworkEstimatorTest.java
@@ -1,6 +1,7 @@
 package fr.dynamo.test;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.amd.aparapi.Range;
@@ -16,23 +17,40 @@ import fr.dynamo.threading.DynamoKernel;
 
 public class NetworkEstimatorTest {
 
-  @Test
-  public void testGetSize() {
+  DynamoKernel kernel;
+  int[] x = new int[1000000];
+  int[][][] z = new int[100][1000][10];
+
+  @Before
+  public void prepare(){
     OpenCLDevice device = new OpenCLDevice(new OpenCLPlatform(), 1, TYPE.CPU);
+
     Range range = new Range(device, 1);
-    DynamoKernel kernel = new DynamoKernel(new DynamoJob("Test"), range) {
 
-      private int[] x = new int[1000000];
-      private int[][][] z = new int[100][1000][10];
 
+
+    kernel = new DynamoKernel(new DynamoJob("Test"), range) {
+      int[] x_input = x;
+      int[][][] z_input = z;
       @Override
       public void run() {
       }
     };
+  }
 
+  @Test
+  public void testGetSize() {
     assertEquals(8000000, kernel.getDataSize());
     assertEquals(6400, NetworkEstimator.calculateTranferTime(kernel, NetworkSpeed.MBIT10));
     assertEquals(6, NetworkEstimator.calculateTranferTime(kernel, NetworkSpeed.GBIT10));
+  }
 
+  @Test
+  public void testTransferredDataSize() {
+    kernel.setExplicit(true);
+    kernel.put(x);
+    assertEquals(4000000, kernel.getTransferredDataSize());
+    kernel.get(x);
+    assertEquals(8000000, kernel.getTransferredDataSize());
   }
 }
